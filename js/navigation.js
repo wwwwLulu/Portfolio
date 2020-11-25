a471c980ad56a7648e205d7bc41c3df2b33b2462
Zallyy <Zallybags@gmail.com>
2020-11-25 07:09:25 -0800

    Navigation done

---
+++ js/navigation.js
@@ -0,0 +1,9 @@
+const checkbox = document.getElementById('navi-toggle')
+
+const nav_link = document.querySelectorAll('.navigation__item')
+
+nav_link.forEach(link => {
+    link.addEventListener('click', () => {
+        checkbox.checked = false
+    })
+})
